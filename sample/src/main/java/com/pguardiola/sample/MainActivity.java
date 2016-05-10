2162dde74043d283dfe4b85a2cb4ebe6a006de20
pgs <pgs@tid.es>
2016-05-10 10:18:21 +0200

    refactor naming and reformat code

--- sample/src/main/java/com/pguardiola/sample/MainActivity.java
+++ sample/src/main/java/com/pguardiola/sample/MainActivity.java
@@ -21,77 +21,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import android.view.View;
 import android.widget.Toast;
 import com.pguardiola.CustomItemClickListener;
 import com.pguardiola.HexAdapter;
 import com.pguardiola.SpacesItemDecoration;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
   private static final int SPAN_COUNT = 2;
-  private RecyclerView foldersView;
-  private RecyclerView.Adapter foldersAdapter;
+  private RecyclerView tilesView;
+  private RecyclerView.Adapter tilesAdapter;
   private GridLayoutManager layoutManager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    foldersView = (RecyclerView) findViewById(R.id.folders);
-
-    foldersView.setHasFixedSize(true);
+    tilesView = (RecyclerView) findViewById(R.id.tiles);
+    tilesView.setHasFixedSize(true);
 
     int squareSideLengthInPixels = getResources().getDimensionPixelSize(R.dimen.square_side_length);
     float radius = squareSideLengthInPixels / 2;
     float adjacent = (float) (Math.sqrt(3) * radius / 2);
-
     int spacingInPixels = -(Math.round(radius - adjacent));
+    tilesView.addItemDecoration(new SpacesItemDecoration(spacingInPixels));
 
-    foldersView.addItemDecoration(new SpacesItemDecoration(spacingInPixels));
-
-    layoutManager = new GridLayoutManager(this, SPAN_COUNT, LinearLayoutManager.VERTICAL, false);
-    layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+    GridLayoutManager.SpanSizeLookup spanSeries = new GridLayoutManager.SpanSizeLookup() {
       @Override public int getSpanSize(int position) {
         return position % 3 == 0 ? 2 : 1;
       }
-    });
+    };
+    layoutManager = new GridLayoutManager(this, SPAN_COUNT, LinearLayoutManager.VERTICAL, false);
+    layoutManager.setSpanSizeLookup(spanSeries);
 
-    foldersView.setLayoutManager(layoutManager);
+    tilesView.setLayoutManager(layoutManager);
 
-    final List<String> folders = new ArrayList<String>() { {
-      add("Foo");
-      add("test");
-      add("folDER");
-      add("Foo");
-      add("test");
-      add("folDER");
-      add("Foo");
-      add("test");
-      add("folDER");
-      add("Foo");
-      add("test");
-      add("folDER");
-      add("Foo");
-      add("test");
-      add("folDER");
-      add("Foo");
-      add("test");
-      add("folDER");
-      add("Foo");
-      add("test");
-      add("folDER");
+    final List<String> tiles = new ArrayList<String>() { {
+      add("8");
+      add("5");
+      add("3");
+      add("4");
+      add("2");
+      add("11");
+      add("6");
+      add("9");
+      add("12");
+      add("10");
+      add("6");
+      add("8");
+      add("9");
+      add("5");
+      add("11");
+      add("4");
+      add("10");
+      add("3");
     } };
-    foldersAdapter = new HexAdapter(folders, new CustomItemClickListener() {
-      @Override
-      public void onItemClick(View v, int position) {
-        Toast.makeText(getApplicationContext(), folders.get(position), Toast.LENGTH_SHORT).show();
+    CustomItemClickListener onClickListener = new CustomItemClickListener() {
+      @Override public void onItemClick(View v, int position) {
+        Toast.makeText(getApplicationContext(), tiles.get(position), Toast.LENGTH_SHORT).show();
       }
-    });
-    foldersView.setAdapter(foldersAdapter);
+    };
+    tilesAdapter = new HexAdapter(tiles, onClickListener);
+    tilesView.setAdapter(tilesAdapter);
   }
 }
